c2280b2cec5319b792aff8d180bf893a63b57c11
Nirmal Patel <patelnirmal1325@gmail.com>
2021-07-20 22:06:03 -0400

    added credit_card_validator.cpp

---
+++ credit_card_validator.cpp
@@ -0,0 +1,11 @@
+/**
+ * This program determines whether the user-input 
+ * credit card number is valid or not, and returns 
+ * an output statement accordingly. 
+ *
+ * @credit_card_validator.cpp 
+ * 
+ * @author Nirmal Patel
+ * @version 1.0 
+ **/
+
