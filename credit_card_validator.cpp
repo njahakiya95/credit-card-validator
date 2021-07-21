af5c300eadb3c9a16a622b4dc68b29b8b2f88fd7
Nirmal Patel <patelnirmal1325@gmail.com>
2021-07-20 23:17:59 -0400

    updated credit_card_validator with pseudocode

--- credit_card_validator.cpp
+++ credit_card_validator.cpp
@@ -9,3 +9,14 @@
  * @version 1.0 
  **/
 
+#include <iostream>
+
+using namespace std; 
+
+//General outline 
+//Determine if the entered credit card length is valid or not
+//Take every other digit starting from back and multiply by 2
+//If any result is greater than 10 add the two numbers 
+//Add the new numbers with the remaining numbers you skipped over 
+//Take sum and perform modulo 10. If the result is even, the credit
+//card is valid 
